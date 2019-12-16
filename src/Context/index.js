89bf946e8221fb3fdacd9f40160a016d09620f89
Gabriel Davi Silva Pereira <gdavi.gd@gmail.com>
2019-12-16 13:15:32 -0300

    Update index.js

--- src/Context/index.js
+++ src/Context/index.js
@@ -4,14 +4,14 @@ import { reducer } from "../reducers/index";
 export const Context = createContext();
 
 export default function ContextProvider(props) {
-  const [state, dispatch] = useReducer(reducer, [], () => {
+  const [modal, dispatch] = useReducer(reducer, [], () => {
     return {
       action: "openModal"
     };
   });
 
   return (
-    <Context.Provider value={{ state, dispatch }}>
+    <Context.Provider value={{ modal, dispatch }}>
       {props.children}
     </Context.Provider>
   );
